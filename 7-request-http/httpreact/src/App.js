7bf357bb6d9335d91ecb64b81bc3707f6aab6764
iagosm <iagosousa201486@gmail.com>
2025-07-31 22:34:41 -0300

    Finalização do modulo de request http com react

--- 7-request-http/httpreact/src/App.js
+++ 7-request-http/httpreact/src/App.js
@@ -1,22 +1,27 @@
 import './App.css';
 import { useState, useEffect } from 'react'
+import { useFetch } from './hooks/useFetch';
 
-const url = "http://localhost:3000/products";
+
+const url = "http://localhost:4000/products";
 
 function App() {
   const [products, setProducts] = useState([]);
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
 
+  // 4 custom hook 
+  const { data : items, httpConfig, loading, error } = useFetch(url);
   // 1 - Resgatando dados
-  useEffect(() => {
-    async function fetchData() {
-      const res = await fetch(url);
-      const data = await res.json();
-      setProducts(data);
-    }
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const res = await fetch(url);
+  //     const data = await res.json();
+  //     setProducts(data);
+  //   }
+  //   fetchData();
+  // }, []);
+
 
 
   // 2 add produtos
@@ -28,41 +33,63 @@ function App() {
       price
     }
 
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(product),
-    });
-    // 3 - Carregamento dinamico
-    const addedProduct = await res.json();
-    setProducts((prev) => [...prev, addedProduct]);
+    // const res = await fetch(url, httpConfig);
+    // // 3 - Carregamento dinamico
+    // const addedProduct = await res.json();
+    // setProducts((prev) => [...prev, addedProduct]);
+    // 5 Refatorando post
+    httpConfig(product, "POST");
     setName("");
     setPrice("");
+  }
+
+  const handleDelete = async(id) => {
+    httpConfig(id, 'DELETE')
   }
   
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>{product.name} - R$: {product.price}</li>
+      {/* 6 Loading */}
+      {loading && <p>Carregando</p>}
+      {error && <p>{error}</p>}
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+      {!error && (
+      <ul style={{ border: '1px solid #ccc', padding: '20px'}}>
+        {items && items.map((product) => (
+           <li
+            key={product.id}
+            style={{
+              display: 'flex',
+              gap: '10px',
+              alignItems: 'center',
+              padding: '8px',
+              listStyle: 'none',
+              borderBottom: '1px solid #ccc'
+            }}
+          >
+            <span>{product.name} - R$: {product.price}</span>
+            <button onClick={() => handleDelete(product.id)}>Deletar</button>
+          </li>
         ))}
-        <div className="add-product">
-          <form onSubmit={handleSubmit}>
-            <label>
-              <span>Nome:</span>
-              <input type="text" value={name} name="name" onChange={(e) => setName(e.target.value)}  />
-            </label>
-            <label>
-              <span>Preço:</span>
-              <input type="number" value={price} name="price" onChange={(e) => setPrice(e.target.value)}  />
-            </label>
-            <input type="submit" value="Criar" />
-          </form>
-        </div>
       </ul>
+      )}
+      </div>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            <span>Nome:</span>
+            <input type="text" value={name} name="name" onChange={(e) => setName(e.target.value)}  />
+          </label>
+          <label>
+            <span>Preço:</span>
+            <input type="number" value={price} name="price" onChange={(e) => setPrice(e.target.value)}  />
+          </label>
+          {/* 7 State Loading */}
+          {loading && <input type="submit" disabled value="Aguarde" />}
+          {!loading && <input type="submit" value="Criar" />}
+        </form>
+      </div>
     </div>
   );
 }
--- 7-request-http/httpreact/src/hooks/useFetch.js
+++ 7-request-http/httpreact/src/hooks/useFetch.js
@@ -4,7 +4,81 @@ import {useState, useEffect} from 'react';
 export const useFetch = (url) => {
   const [data, setData] = useState(null)
 
+  // 5 refatorando  post
+  const [config, setConfig] = useState(null);
+  const [method, setMethod] = useState(null);
+  const [callFetch, setCallFetch] = useState(false);
+
+  // 6 loading
+  const [loading, setLoading] = useState(false);
+
+  /// 7 Tratando erros
+  const [error, setError] = useState(null)
+
+  // 8 Desafio 6
+  const [itemId, setItemId] = useState(null)
+
+
+  const httpConfig = (data, method) => {
+    if(method === "POST") {
+      setConfig({
+        method,
+        headers: {
+          'Content-type' : "application/json"
+        },
+        body: JSON.stringify(data)
+      });
+      setMethod(method);
+    }
+    if(method === 'DELETE') {
+      setConfig({
+        method,
+        headers: {
+          'Content-type' : "application/json"
+        }
+      });
+      setMethod(method);
+      setItemId(data);
+    }
+  }
+
   useEffect(() => {
-    
-  }, [url])
-}
+    const fetchData = async () => {
+      setLoading(true);
+
+      try{
+        const res = await fetch(url)
+        const json = await res.json();
+        setData(json);
+        setLoading(false);
+        console.log(json)
+      } catch(error) {
+        console.log(error.message)
+        setError(error.message);
+      }
+    };
+    fetchData();
+  }, [url, callFetch]);
+  
+  
+  //5 Refatorando post
+  useEffect(() => {
+    const httpRequest = async () => {
+      let json;
+      if(method === "POST"){
+        let fetchOptions = [url, config];
+        const res = await fetch(...fetchOptions);
+        json = await res.json();
+        setCallFetch(json);
+        console.log(json)
+      } else if(method === "DELETE"){
+        const deleteUrl = `${url}/${itemId}`
+        const res = await fetch(deleteUrl, config);
+        json = await res.json();
+      }
+      setCallFetch(json);
+    }
+    httpRequest();
+  }, [config, method, url]);
+  return {data, httpConfig, loading, error};
+};
